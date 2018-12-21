04cf834cdbea1edca57f025be8d8d26e7f229708
bereket6725 <bereket6725@gmail.com>
2018-12-20 22:45:32 -0500

    added Presenter with a registration delegate

--- RegistrationPage-MVP/RegistrationPage-MVP/RegistrationPresenter.swift
+++ RegistrationPage-MVP/RegistrationPage-MVP/RegistrationPresenter.swift
@@ -8,6 +8,25 @@
 
 import Foundation
 
+protocol RegistrationDelegate{
+    func showProgress()
+    func hideProgress()
+    func registrationDidSucceed()
+    func registrationDidFail(message: String)
+}
+
 class RegistrationPresenter {
     
+    var delegate: RegistrationDelegate
+    
+    init(delegate: RegistrationDelegate){
+        self.delegate = delegate
+    }
+    
+    func register(email: String, password: String,  fullName: String, phoneNumber: String){
+        if email.isEmpty {
+            print("cant register without an email")
+            self.delegate.registrationDidFail(message: "Didnt input email")
+        }
+    }
 }
