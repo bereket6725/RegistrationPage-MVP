697d671b95354212990f5b8e9762cc6fae237aba
bereket6725 <bereket6725@gmail.com>
2018-12-20 22:52:37 -0500

    added ViewController logic

--- RegistrationPage-MVP/RegistrationPage-MVP/ViewController.swift
+++ RegistrationPage-MVP/RegistrationPage-MVP/ViewController.swift
@@ -15,16 +15,38 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var FullNameTextField: UITextField!
     @IBOutlet var PhoneNumberTextField: UITextField!
     
+    var presenter: RegistrationPresenter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func registrationButtonTapped(_ sender: Any) {
+        self.presenter?.register(email: EmailTextField.text!, password: PasswordTextField.text!, fullName: FullNameTextField.text!, phoneNumber: PhoneNumberTextField.text!)
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: RegistrationDelegate {
+    func showProgress() {
+        <#code#>
+    }
+    
+    func hideProgress() {
+        <#code#>
+    }
+    
+    func registrationDidSucceed() {
+        print("succeed")
+    }
+    
+    func registrationDidFail(message: String) {
+        print(message)
+    }
 }
 
